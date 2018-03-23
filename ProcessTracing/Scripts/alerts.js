4ceee4df931e28b515916c6b00761862695faf86
Bartosz Przydatek <bartoszprzydatek1995@gmail.com>
2018-03-23 23:44:05 +0100

    Repair responsiveness and add alert

---
+++ ProcessTracing/Scripts/alerts.js
@@ -0,0 +1,20 @@
+﻿function showNewActivityAlert() {
+    $(document).ready(function () {
+
+        $('#someBtn').click(function () {
+            $('#newActivityAlert').show('fade');
+            $('#newActivityImg').show('fade');
+            $('#someBtn').hide('fade');
+        });
+    });
+}
+
+function closeNewActivityAlert() {
+    $(document).ready(function () {
+        $('#linkClose').click(function () {
+            $('#newActivityAlert').hide('fade');
+            $('#newActivityImg').hide('fade');
+            $('#someBtn').show('fade');
+        })
+    });
+}
